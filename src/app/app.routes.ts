3c21dca6ed987a7536e4965cd6727c840809133d
Erik <beltraberik@gmail.com>
2025-06-28 01:23:23 -0600

    tsconfig fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/user/auth.guard';
-import { LoginComponent } from './features/public/login/login.component';
+import { LoginComponent } from '@features/public/login/login.component';
 import { PrivateLayoutComponent } from './layouts/private-layout/private-layout.component';
 
 export const routes: Routes = [
