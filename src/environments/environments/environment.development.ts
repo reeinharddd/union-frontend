c049b3a2a5550a40dd4010d72deb0f16ceff9e94
jesuslab135 <jefeing.esteban@gmail.com>
2025-08-07 11:19:42 -0700

    hola

---
+++ src/environments/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'xerothermic-donni-union-67b28843.koyeb.app/', // Esto lo resuelve Vercel
+  debugMode: false,
+  mockBackend: false,
+};
