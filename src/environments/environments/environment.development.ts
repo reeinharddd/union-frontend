35d73477a91103b183e84f618b0dab8440b9c3f9
jesuslab135 <jefeing.esteban@gmail.com>
2025-08-07 11:47:10 -0700

    Revert "hola"
    This reverts commit c049b3a2a5550a40dd4010d72deb0f16ceff9e94.

--- src/environments/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'xerothermic-donni-union-67b28843.koyeb.app/', // Esto lo resuelve Vercel
-  debugMode: false,
-  mockBackend: false,
-};
